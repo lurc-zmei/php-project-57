31573d17c90b2e048f8236bbe4d4a6d8f9f160b4
WinDon <lurc_zmei@inbox.ru>
2026-08-18 10:54:43 +0300

    refactor: replace base welcome view with x-layout component

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        Менеджер задач
+    </x-slot:title>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3xl font-bold">Привет от Хекслета!</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
